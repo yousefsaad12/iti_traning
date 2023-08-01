9c840e8495c35aa5955050db806cc2bf0d39bd01
Yousef saad <saadrslan95@gmail.com>
2023-08-01 07:48:18 +0300

    Day3 and Day2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MoviesPageComponent } from './movies-page/movies-page.component';
 
-const routes: Routes = [];
+import { SeriesPageComponent } from './series-page/series-page.component';
+import { MovieDetialsComponent } from './movie-detials/movie-detials.component';
+import { SeriesDetailsComponent } from './series-details/series-details.component';
+
+const routes: Routes = 
+[
+  {path:'home', component:HomeComponent},
+  {path:'movies', component:MoviesPageComponent},
+  {path: 'series', component:SeriesPageComponent},
+  {path: 'movie_details/:id', component:MovieDetialsComponent},
+  {path: 'series-details/:id', component:SeriesDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
 import { CardsComponent } from './cards/cards.component';
 import { FooterComponent } from './footer/footer.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { HomeComponent } from './home/home.component';
+import { MoviesPageComponent } from './movies-page/movies-page.component';
+import { SeriesCardComponent } from './series-card/series-card.component';
+import { SeriesPageComponent } from './series-page/series-page.component';
+import { MovieDetialsComponent } from './movie-detials/movie-detials.component';
+import { SeriesDetailsComponent } from './series-details/series-details.component';
+
+
 
 @NgModule({
   declarations: [
@@ -14,11 +23,21 @@ import { FooterComponent } from './footer/footer.component';
     HeaderComponent,
     BannerComponent,
     CardsComponent,
-    FooterComponent
+    FooterComponent,
+    SearchBarComponent,
+    HomeComponent,
+    MoviesPageComponent,
+    SeriesCardComponent,
+    SeriesPageComponent,
+    MovieDetialsComponent,
+    SeriesDetailsComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
